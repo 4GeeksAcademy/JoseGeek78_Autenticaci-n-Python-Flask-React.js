733fbfd4444fb7ddafe32e969e9132197a71c363
Jose <126101249+JoseGeek78@users.noreply.github.com>
2023-12-19 18:51:09 +0100

    Updated single.js

--- src/front/js/pages/single.js
+++ src/front/js/pages/single.js
@@ -1,28 +1,92 @@
-import React, { useState, useEffect, useContext } from "react";
-import PropTypes from "prop-types";
-import { Link, useParams } from "react-router-dom";
+import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+import { useNavigate, Link } from "react-router-dom";
+import "../../styles/home.css";
 
-export const Single = props => {
-	const { store, actions } = useContext(Context);
-	const params = useParams();
+export const Signup = () => {
+  const { store, actions } = useContext(Context);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-	return (
-		<div className="jumbotron">
-			<h1 className="display-4">This will show the demo element: {store.demo[params.theid].title}</h1>
-			<img src={rigoImageUrl} />
-			<hr className="my-4" />
+  const navigate = useNavigate();
 
-			<Link to="/">
-				<span className="btn btn-primary btn-lg" href="#" role="button">
-					Back home
-				</span>
-			</Link>
-		</div>
-	);
-};
+  console.log("this is your token", store.token);
 
-Single.propTypes = {
-	match: PropTypes.object
+  const handleClick = (e) => {
+    e.preventDefault();
+    actions.register(email, password);
+    actions.setRegistrationInProgress(true);
+
+    // CONDITIONALS with FRONTEND messages (below in the jsx):
+    setTimeout(() => {
+      actions.setRegistrationInProgress(false);
+      if (store.registrationSuccess) {
+        actions.setRegistrationSuccess(false);
+        navigate("/login");
+      }
+      actions.setRegistrationExists(false);
+      actions.setRegistrationEmpty(false);
+    }, 2000);
+  };
+
+  return (
+    <div className="text-center mt-5 home_max-width container">
+      {!store.registrationSuccess && <h1>Create account</h1>}
+
+      {store.registrationEmpty && (
+        <div className="fs-3">
+          Email and password are required.
+          <br />
+          Try again!
+        </div>
+      )}
+
+      {store.registrationExists && (
+        <div className="fs-3">
+          Sorry!
+          <br />
+          That user already exists!
+        </div>
+      )}
+
+      {store.registrationSuccess && (
+        <div className="fs-3">
+          You are successfully registered!
+          <br />
+          Now you'll be redirected so you can <b>log in</b>!
+        </div>
+      )}
+
+      <div>
+        <form onSubmit={handleClick}>
+          <input
+            type="email"
+            placeholder="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="form-control mt-3"
+          />
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="form-control mt-3"
+          />
+          <button
+            type="submit"
+            className="btn btn-dark mt-5"
+            disabled={store.registrationInProgress}
+          >
+            Sign up
+          </button>
+        </form>
+
+        <p className="mt-3 mb-5">
+          You already have an account? <Link to="/login"> Log In then </Link>
+        </p>
+      </div>
+      {/* )} */}
+    </div>
+  );
 };
